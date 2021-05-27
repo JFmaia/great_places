84590bd683bc048cedfee6db3fd2ca7e95c6b05d
José Flávio Maia <makarovmaia@gmail.com>
2021-05-27 08:43:02 -0300

    create folder and files

---
+++ lib/views/place_form_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PlaceFormScreen extends StatefulWidget {
+  @override
+  _PlaceFormScreenState createState() => _PlaceFormScreenState();
+}
+
+class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [],
+        title: Text("Novo lugar"),
+      ),
+      body: Center(
+        child: Text("Form!!!"),
+      ),
+    );
+  }
+}
