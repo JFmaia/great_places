926ae7a73c967f766e1a5f883d991523d297245e
JFmaia <jfmaia741@gmail.com>
2021-06-14 09:38:49 -0300

    Modified the code od file.

--- lib/views/places_list_screen.dart
+++ lib/views/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:great_places/controller/great_places.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatefulWidget {
   @override
@@ -21,8 +23,22 @@ class _PlacesListScreenState extends State<PlacesListScreen> {
           ),
         ],
       ),
-      body: Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        builder: (context, greatPlaces, _) => greatPlaces.itemsCount == 0
+            ? Center(
+                child: Text('Nenhum local cadastrado!'),
+              )
+            : ListView.builder(
+                itemCount: greatPlaces.itemsCount,
+                itemBuilder: (ctx, i) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        FileImage(greatPlaces.itemByIndex(i).image),
+                  ),
+                  title: Text(greatPlaces.itemByIndex(i).title),
+                  onTap: () {},
+                ),
+              ),
       ),
     );
   }
