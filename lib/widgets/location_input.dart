f64464042d5f38182eba63c5de3cb0685911e2f4
JFmaia <jfmaia741@gmail.com>
2021-06-15 10:38:08 -0300

    new file
    Nesse aquivo criado estar o widget para selecionar no mapa a localização ou ser capturada automaticamente.

---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/utils/location_util.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({Key? key}) : super(key: key);
+
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String? _previewImageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+    final staticMapImageUrl = LocationUtil.generateLocationPreviewImage(
+        latitude: locData.latitude!, longitude: locData.longitude!);
+
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            border: Border.all(width: 1, color: Colors.grey),
+          ),
+          child: _previewImageUrl != null
+              ? Image.network(
+                  _previewImageUrl!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text('Localização não Informada'),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              icon: Icon(Icons.location_on),
+              label: Text(
+                "Localização Atual",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              onPressed: _getCurrentUserLocation,
+            ),
+            TextButton.icon(
+              icon: Icon(
+                Icons.map,
+                color: Theme.of(context).primaryColor,
+              ),
+              label: Text(
+                "Selecione no Mapa",
+                style: TextStyle(color: Theme.of(context).primaryColor),
+              ),
+              onPressed: () {},
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
